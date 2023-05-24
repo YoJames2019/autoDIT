2f09913aacead35c1d388528da15db6d1bc989cb
James Clark <jamesclark@pop-os.localdomain>
2023-05-24 18:08:49 -0400

    added proxy and audio moving functionality

--- python/main.py
+++ python/main.py
@@ -1,10 +1,12 @@
 import os
+import sys
 import subprocess
 import numpy as np
 import cv2
 import math
 import fnmatch
 from ultralytics import YOLO
+import shutil
 
 num_conversions = {
     'one': 1,
@@ -19,48 +21,76 @@ num_conversions = {
     'zero': 0
 }
 
-video_dir = "./test_media/videos/"
+# check if the correct arguments were input
+if len(sys.argv) < 4:
+    print(f"Invalid syntax! Correct syntax is: \n{sys.executable} {os.path.abspath(__file__)} \"replace/with/path/to/folder/with/videos/\" \"replace/with/path/to/folder/with/audio/\" \"replace/with/path/to/output/folder/\"")
+    exit()
+
+# define the input video, input audio, and output directory variables from the arguments
+input_video_dir = sys.argv[1]
+input_audio_dir = sys.argv[2]
+output_video_dir = sys.argv[3]
+
+# check if both of those paths exist and if they don't, error
+for path in sys.argv[1:4]:
+    if not os.path.exists(path):
+        print(f"\nThe folder \"{path}\" does not exist!")
+        exit()
+
+# Instantiate the clapboard and clapboard text models
 clapboard_model = YOLO("./yolo_v8/weights_final/clapboard_best.pt")
 clapboard_text_model = YOLO("./yolo_v8/weights_final/clapboard_text_best.pt")
 
 def main():
     # loop through all videos in a certain directory and all subdirectories
-    videopaths = find_video_files(video_dir)
+    videopaths = find_video_files(input_video_dir)
     # pass each video to a parser that will get the first 10 seconds (240 frames) of the video (and if tail=True it will get the last 240 frames of the video)
     for videopath in videopaths:
-        print(videopath + " - ")
+        print(f"\n{videopath} - ")
+        filename = os.path.basename(videopath)
+        proxy = "_proxy" in filename
+
         frames = extract_frames(videopath, 10, 800, 800)
         
-        print(videopath + " - Checking for clapboard")
+        print(f"{videopath} - Checking for clapboard")
         detected = detect_best_clapboard_info(frames)
-        # for clapboard in detected:
-        #     print(clapboard['confs'])
-        #     print(clapboard['boxes'])
+
         # if it cannot find a clapboard, find text, and validate the text:
         if detected is None:
-            print(videopath + " - No clapboard detected, checking for tailslate")
+            print(f"{videopath} - No clapboard detected, checking for tailslate")
             # it will pass the video back to the parser with tail=True
             frames = extract_frames(videopath, 10, 800, 800, True)
             detected = detect_best_clapboard_info(frames)
         
-        scene, cam, shot, take = detected['results']
-        # print(detected['conf'])
-        print(detected['results'])
-        # print(detected['boxes'])
+        # if it still couldnt find a clapboard, set all the below variables to none 
+        scene, cam, shot, take = detected['results'] if detected else [None, None, None, None]
 
-        print(videopath + " - Found Clapboard Data:")
-        print("    Scene: " + scene)
-        print("    Camera: " + cam)
-        print("    Shot: " + shot)
-        print("    Take: " + take)
+        print(f"{videopath} - Found Clapboard Data:")
+        print("    Scene: " + str(scene))
+        print("    Camera: " + str(cam))
+        print("    Shot: " + str(shot))
+        print("    Take: " + str(take))
 
-        cv2.imshow("detected", detected['frame_plotted'])
-        cv2.waitKey(0)
         # if it found all this correctly, it will then check
         # if the video file name is the same as predicted:
-        #   move the video to its correct directory
-        #   rename and move the corresponding audio file to the correct directory
-        # if not, move the video file to the "Manual Review Required" folder
+        if f"s{scene}_s{shot}_t{take}." in filename.lower() or f"s{scene}_s{shot}_t{take}_proxy." in filename.lower():
+            print(f"{videopath} - Correct filename")
+
+            # move the video to its correct directory
+            copy_file_safe(videopath, os.path.join(output_video_dir, f"Scene {scene}/{'Proxy' if proxy else '4K'}"))
+            
+            # find the corresponding audio file from the input audio directory
+            audio_filepath = search_dir(input_audio_dir, f"{int(scene)}{chr(int(shot) + ord('a') - 1).upper()}_T{int(take)}.wav")
+            # rename and move the corresponding audio file to the correct directory
+            if audio_filepath is not None:
+                copy_file_safe(audio_filepath, os.path.join(output_video_dir, f"Scene {scene}/Audio"), f"S{scene}_S{shot}_T{take}{os.path.splitext(audio_filepath)[1]}")
+
+        else:
+            # if not, move the video file to the "Manual Review Required" directory
+            print(f"{videopath} - Incorrect filename")
+            
+            copy_file_safe(videopath, os.path.join(output_video_dir, "Manual Review Required"))
+
 
 
 def find_video_files(directory, extensions=['*.mp4', '*.mov']):
@@ -76,6 +106,13 @@ def find_video_files(directory, extensions=['*.mp4', '*.mov']):
         print(f"Current working directory: {os.getcwd()}")
         print(f"Absolute path: {os.path.abspath(directory)}")
     return video_files
+
+def search_dir(start_dir, target_file):
+    for dirpath, dirnames, filenames in os.walk(start_dir):
+        for filename in filenames:
+            if filename == target_file:
+                return os.path.join(dirpath, filename)
+    return None
 
 def detect_best_clapboard_info(frames):
     # for each frame of that video, pass that frame to the clapboardrecognition class which will then:
@@ -137,10 +174,10 @@ def detect_best_clapboard_info(frames):
         else:
             return None
 
-def extract_frames(video_path, sec, width, height, tail=False):
+def extract_frames(videopath, sec, width, height, tail=False):
     # Get the duration of the video
-    print(video_path + " - Checking video duration")
-    duration_cmd = ['ffprobe', '-v', 'error', '-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1', video_path]
+    print(f"{videopath} - Checking video duration")
+    duration_cmd = ['ffprobe', '-v', 'error', '-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1', videopath]
     duration = float(subprocess.check_output(duration_cmd))
 
     # Calculate the start and end times
@@ -152,17 +189,35 @@ def extract_frames(video_path, sec, width, height, tail=False):
         end_time = min(sec, duration)
     
     # Extract frames from the video
-    print(video_path + " - Extracting frames")
-    extract_cmd = ['ffmpeg', '-i', video_path, '-ss', str(start_time), '-t', str(end_time - start_time), '-vf', f'scale=w={width}:h={height}:force_original_aspect_ratio=decrease,pad={width}:{height}:(ow-iw)/2:(oh-ih)/2' + (',vflip' if tail else ''), '-q:v', '2', '-f', 'image2pipe', '-pix_fmt', 'rgb24', '-vcodec', 'rawvideo', '-']
+    print(f"{videopath} - Extracting frames")
+    extract_cmd = ['ffmpeg', '-i', videopath, '-ss', str(start_time), '-t', str(end_time - start_time), '-vf', f'scale=w={width}:h={height}:force_original_aspect_ratio=decrease,pad={width}:{height}:(ow-iw)/2:(oh-ih)/2' + (',vflip,hflip' if tail else ''), '-q:v', '2', '-f', 'image2pipe', '-pix_fmt', 'rgb24', '-vcodec', 'rawvideo', '-']
     result = subprocess.run(extract_cmd, stderr=subprocess.DEVNULL, stdout=subprocess.PIPE)
     frames_raw = result.stdout
     
-    print(video_path + " - Converting buffer to numpy array")
+    print(f"{videopath} - Converting buffer to numpy array")
     # Convert the frame buffer to a numpy array
     frames = np.frombuffer(frames_raw, dtype=np.uint8)
     frames = frames.reshape((-1, height, width, 3))
     
     return frames
+
+def copy_file_safe(filepath, dest_dir, filename=None, attempt=1):
+    if filename is None:
+        filename = os.path.basename(filepath)
+
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
+
+    dest_filepath = os.path.join(dest_dir, filename)
+    try:
+        shutil.copy2(filepath, dest_filepath)
+    except Exception as e:
+        if attempt <= 5:
+            att += 1;
+            copy_file_safe(filepath, dest_dir, filename, attempt)
+        
+
+        
 
 def calculate_distance(point1, point2):
     x1, y1 = point1
